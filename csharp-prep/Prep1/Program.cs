a08951c069cab18e04d697895e255ab5d39059fc
constence <hol20087@byui.edu>
2025-01-10 11:32:20 -0700

    james bond project

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First name?"); 
+        string first_name = Console.ReadLine(); 
+
+        Console.Write("What is your Last name?"); 
+        string last_name = Console.ReadLine(); 
+
+        Console.WriteLine($"{last_name}, {first_name} {last_name}");
     }
 }
