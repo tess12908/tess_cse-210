807adf97e00eb1f8c9ecb323f1b31ec43cb18c19
constence <hol20087@byui.edu>
2025-01-10 11:53:36 -0700

    grade percent and jason born activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         Console.Write("What is your First name?"); 
         string first_name = Console.ReadLine(); 
 
@@ -12,5 +13,53 @@ class Program
         string last_name = Console.ReadLine(); 
 
         Console.WriteLine($"{last_name}, {first_name} {last_name}");
+        */
+
+        Console.Write("What is your grade percentage?"); 
+        string grade = Console.ReadLine(); 
+        int grade_perc = int.Parse(grade); 
+
+        int a = 90; 
+        int b = 80; 
+        int c = 70; 
+        int d = 60; 
+        int f = 60;
+        string letter_grade = ""; 
+
+
+        if (grade_perc >= a)
+        {
+            letter_grade = "A"; 
+        }
+
+        else if (grade_perc >= b)
+        {
+            letter_grade = "B"; 
+        }
+
+        else if (grade_perc >= c)
+        {
+            letter_grade = "C"; 
+        }
+
+        else if (grade_perc >= d)
+        {
+           letter_grade = "D"; 
+        }
+
+        else if (grade_perc < f)
+        {
+           letter_grade = "F"; 
+        }
+
+
+        if (grade_perc >= c)
+        {
+            Console.WriteLine("You passed! You got at least a C"); 
+        }
+        else 
+        {
+             Console.WriteLine("You did not pass! You got below a C"); 
+        }
     }
 }
